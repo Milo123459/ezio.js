cbef4f24063d64eab0bf3a16251d13c4dac795aa
Shubham Parihar <shubhamparihar391@gmail.com>
2020-12-05 00:26:33 +0530

    added index.js, readme, and gitignore files

---
+++ README.md
@@ -0,0 +1,3 @@
+A promise based simple Input-Output module for JavaScript.
+
+<!-- Will write more after I wake up... -->
---
+++ index.js
@@ -0,0 +1,35 @@
+import { createInterface } from 'readline';
+
+
+export default class Ezio {
+
+    // asks a question to the user, waits for the user's input, and then returns the input as a promise
+    ask(question) {
+        return new Promise((resolve, reject) => {
+            let input = null;
+
+            const ioInterface = createInterface({
+                input: process.stdin,
+                output: process.stdout,
+                prompt: '',
+            });
+
+            ioInterface.question(question, (answer) => {
+                input = answer;
+                ioInterface.close();
+            });
+
+            setInterval(() => {
+                if (input) {
+                    resolve(input);
+                }
+            }, 100);
+        });
+    }
+
+
+    // why not? ¯\_(ツ)_/¯
+    print(message) {
+        console.log(message);
+    }
+}
